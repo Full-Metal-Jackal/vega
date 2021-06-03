34b12f42b21317bdbcc1ba093ec094c320765b04
unknown <apodolyak.99@mail.ru>
2021-06-03 18:52:22 +0700

    Fix code

--- src/Assets/Scripts/Systems/WallsDetection/WallsDetectionManager.cs
+++ src/Assets/Scripts/Systems/WallsDetection/WallsDetectionManager.cs
@@ -1,5 +1,5 @@
 /*
- * Детекция и изменение видимости стен перекрывающих игрока
+ * Detecting walls and changing their material to transparent 
  */
 using System.Collections;
 using System.Collections.Generic;
@@ -9,41 +9,38 @@ public class WallsDetectionManager : MonoBehaviour
 {
     private GameObject player;
     private Camera cam;
-    private GameObject walls; // <TODO> Нужно чтоб при загрузке лвл тэги всех дочерних объектов объекта "Walls" устанавливались на "Detectable"
-    private int layerMask = 1 << 3; // <TODO> ПОнять почему маски не работают
-    // Start is called before the first frame update
+    private GameObject walls; // <TODO> It is necessary that when loading the lvl,
+                              // the tags of all child objects of the "Walls" object are set to " Detectable"
+    private int layerMask = 1 << 3; // <TODO> Understand why masks don't work
     [SerializeField] private string detectableTag = "Detectable";
     [SerializeField] private Material highlighMat;
     [SerializeField] private Material defaultMat;
 
-    private Transform _detection;
+    private Transform curentDetection;
     void Start()
     {
         player = GameObject.Find("Human");
         cam = GameObject.Find("MainCamera").GetComponent<Camera>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Ray ray = cam.ScreenPointToRay(player.transform.position);
         RaycastHit hit;
         if (Physics.Raycast(cam.transform.position, (player.transform.position - cam.transform.position), out hit))
         {
-            if (_detection != null)
+            if (curentDetection != null)
             {
-                Renderer[] selectionRenderer = _detection.GetComponentsInChildren<Renderer>();
+                Renderer[] selectionRenderer = curentDetection.GetComponentsInChildren<Renderer>();
                 for (int i = 0; i < selectionRenderer.Length; i++)
                 {
                     selectionRenderer[i].material = defaultMat;
-                    _detection = null;
+                    curentDetection = null;
                 }
             }
             var detection = hit.transform;
             if (detection.CompareTag(detectableTag))
             {
                 var detectionGr = detection.parent;
-                Debug.Log(detectionGr);
                 Renderer[] selectionRenderer = detectionGr.GetComponentsInChildren<Renderer>();
                 if (selectionRenderer != null)
                 {
@@ -51,17 +48,9 @@ public class WallsDetectionManager : MonoBehaviour
                     {
                         selectionRenderer[i].material = highlighMat;
                     }
-
-                    _detection = detectionGr;
+                    curentDetection = detectionGr;
                 }
-            }
-           
-            
+            }  
         }
-        else
-        {
-            Debug.Log("Nothing hitted");
-        }
-        
     }
 }
