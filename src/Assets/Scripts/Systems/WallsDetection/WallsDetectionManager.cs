03a4b73947d33943b354daa0b61b27517567b26c
unknown <apodolyak.99@mail.ru>
2021-06-03 18:59:05 +0700

    change CRLF to LF (not sure)

--- src/Assets/Scripts/Systems/WallsDetection/WallsDetectionManager.cs
+++ src/Assets/Scripts/Systems/WallsDetection/WallsDetectionManager.cs
@@ -7,50 +7,50 @@ using UnityEngine;
 
 public class WallsDetectionManager : MonoBehaviour
 {
-    private GameObject player;
-    private Camera cam;
-    private GameObject walls; // <TODO> It is necessary that when loading the lvl,
-                              // the tags of all child objects of the "Walls" object are set to " Detectable"
-    private int layerMask = 1 << 3; // <TODO> Understand why masks don't work
-    [SerializeField] private string detectableTag = "Detectable";
-    [SerializeField] private Material highlighMat;
-    [SerializeField] private Material defaultMat;
+	private GameObject player;
+	private Camera cam;
+	private GameObject walls; // <TODO> It is necessary that when loading the lvl,
+							  // the tags of all child objects of the "Walls" object are set to " Detectable"
+	private int layerMask = 1 << 3; // <TODO> Understand why masks don't work
+	[SerializeField] private string detectableTag = "Detectable";
+	[SerializeField] private Material highlighMat;
+	[SerializeField] private Material defaultMat;
 
-    private Transform curentDetection;
-    void Start()
-    {
-        player = GameObject.Find("Human");
-        cam = GameObject.Find("MainCamera").GetComponent<Camera>();
-    }
+	private Transform curentDetection;
+	void Start()
+	{
+		player = GameObject.Find("Human");
+		cam = GameObject.Find("MainCamera").GetComponent<Camera>();
+	}
 
-    void Update()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(cam.transform.position, (player.transform.position - cam.transform.position), out hit))
-        {
-            if (curentDetection != null)
-            {
-                Renderer[] selectionRenderer = curentDetection.GetComponentsInChildren<Renderer>();
-                for (int i = 0; i < selectionRenderer.Length; i++)
-                {
-                    selectionRenderer[i].material = defaultMat;
-                    curentDetection = null;
-                }
-            }
-            var detection = hit.transform;
-            if (detection.CompareTag(detectableTag))
-            {
-                var detectionGr = detection.parent;
-                Renderer[] selectionRenderer = detectionGr.GetComponentsInChildren<Renderer>();
-                if (selectionRenderer != null)
-                {
-                    for (int i = 0; i < selectionRenderer.Length; i++)
-                    {
-                        selectionRenderer[i].material = highlighMat;
-                    }
-                    curentDetection = detectionGr;
-                }
-            }  
-        }
-    }
+	void Update()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(cam.transform.position, (player.transform.position - cam.transform.position), out hit))
+		{
+			if (curentDetection != null)
+			{
+				Renderer[] selectionRenderer = curentDetection.GetComponentsInChildren<Renderer>();
+				for (int i = 0; i < selectionRenderer.Length; i++)
+				{
+					selectionRenderer[i].material = defaultMat;
+					curentDetection = null;
+				}
+			}
+			var detection = hit.transform;
+			if (detection.CompareTag(detectableTag))
+			{
+				var detectionGr = detection.parent;
+				Renderer[] selectionRenderer = detectionGr.GetComponentsInChildren<Renderer>();
+				if (selectionRenderer != null)
+				{
+					for (int i = 0; i < selectionRenderer.Length; i++)
+					{
+						selectionRenderer[i].material = highlighMat;
+					}
+					curentDetection = detectionGr;
+				}
+			}  
+		}
+	}
 }
