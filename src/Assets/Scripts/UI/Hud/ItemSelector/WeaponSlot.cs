4de7142822f04006f57709a5c9a4f23f95cbb71c
Havch1k92 <81084922+Havch1k92@users.noreply.github.com>
2021-06-29 13:19:54 +0300

    добавил неймспейс

--- src/Assets/Scripts/UI/Hud/ItemSelector/WeaponSlot.cs
+++ src/Assets/Scripts/UI/Hud/ItemSelector/WeaponSlot.cs
@@ -2,61 +2,64 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WeaponSlot : MonoBehaviour
+namespace UI
 {
-	[SerializeField]
-	private GameObject slotContainer;
-
-	private RectTransform slotTransform;
-	private Image slotImage;
-	private Text ammoCount;
-	private Gun gun;
-
-	private const float offset = 48;
-
-	private void Awake() => Initialize();
-
-	private void Start()
+	public class WeaponSlot : MonoBehaviour
 	{
-		enabled = false;
-		ammoCount.gameObject.SetActive(false);
+		[SerializeField]
+		private GameObject slotContainer;
 
-		Vector3 origPos = slotTransform.localPosition;
-		slotTransform.localPosition = new Vector3(origPos.x, offset, origPos.z); // initially offset the slot, showing its disabled state
+		private RectTransform slotTransform;
+		private Image slotImage;
+		private Text ammoCount;
+		private Gun gun;
 
-		PlayerController.Instance.Possessed.OnPickedUpItem += (item) =>
+		private const float offset = 48;
+
+		private void Awake() => Initialize();
+
+		private void Start()
 		{
-			if (item is Gun gun)
+			enabled = false;
+			ammoCount.gameObject.SetActive(false);
+
+			Vector3 origPos = slotTransform.localPosition;
+			slotTransform.localPosition = new Vector3(origPos.x, offset, origPos.z); // initially offset the slot, showing its disabled state
+
+			PlayerController.Instance.Possessed.OnPickedUpItem += (item) =>
 			{
-				GameObject iconContainer = gun.ItemData.Icon;
-				Image gunImg = iconContainer.GetComponentInChildren<Image>();
-				if (!gunImg)
-					throw new Exception($"Cannot find {gunImg.GetType()} component in {iconContainer} or its children");
+				if (item is Gun gun)
+				{
+					GameObject iconContainer = gun.ItemData.Icon;
+					Image gunImg = iconContainer.GetComponentInChildren<Image>();
+					if (!gunImg)
+						throw new Exception($"Cannot find {gunImg.GetType()} component in {iconContainer} or its children");
 
-				this.gun = gun;
+					this.gun = gun;
 
-				slotImage.sprite = gunImg.sprite;
-				slotImage.color = Color.white;
-				slotTransform.localPosition = origPos;
+					slotImage.sprite = gunImg.sprite;
+					slotImage.color = Color.white;
+					slotTransform.localPosition = origPos;
 
-				enabled = true;
-				ammoCount.gameObject.SetActive(true);
-			}
-		};
+					enabled = true;
+					ammoCount.gameObject.SetActive(true);
+				}
+			};
+		}
+
+		private void Initialize()
+		{
+			Image[] images = slotContainer.GetComponentsInChildren<Image>();
+			if (images.Length != 2 || !images[1]?.transform.parent)
+				throw new Exception($"{slotContainer} must have an {typeof(Image)} component background with a child {typeof(Image)} component");
+
+			// If the need arises (e.g. to change the background color), images[0] should be the background image
+			slotImage = images[1];
+			ammoCount = slotContainer.GetComponentInChildren<Text>();
+			slotTransform = slotContainer.GetComponent<RectTransform>();
+		}
+
+		private void Update() =>
+			ammoCount.text = gun?.AmmoCount.ToString();
 	}
-
-	private void Initialize()
-	{
-		Image[] images = slotContainer.GetComponentsInChildren<Image>();
-		if (images.Length != 2 || !images[1]?.transform.parent)
-			throw new Exception($"{slotContainer} must have an {typeof(Image)} component background with a child {typeof(Image)} component");
-
-		// If the need arises (e.g. to change the background color), images[0] should be the background image
-		slotImage = images[1];
-		ammoCount = slotContainer.GetComponentInChildren<Text>();
-		slotTransform = slotContainer.GetComponent<RectTransform>();
-	}
-
-	private void Update() =>
-		ammoCount.text = gun?.AmmoCount.ToString();
 }
