762bedcd8de71df517531ba10be134617b44dd3b
Ocelot <ocelotwithmonocle@gmail.com>
2021-07-06 19:15:11 +0700

    универсализированный биллборд для спрайта

---
+++ src/Assets/Scripts/Entities/StaticProps/BillboardSprite.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class BillboardSprite : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        if(Camera.current)
+            transform.forward = Camera.current.transform.forward;
+    }
+}
