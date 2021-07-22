6983cce55bca347df9c0cdbb4f1a2631e3d4d340
Ocelot <ocelotwithmonocle@gmail.com>
2021-07-22 17:17:11 +0700

    добавил зачатки системы триггеров и фразы персонажей вне сюжетных диалогов

---
+++ src/Assets/Scripts/Systems/Trigger/Trigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace TriggerSystem
+{
+	[System.Serializable]
+	public class TriggerAction : UnityEvent
+	{
+	}
+
+	public abstract class Trigger : MonoBehaviour
+	{
+		public TriggerAction OnTriggered;
+
+		public virtual void Activate() => OnTriggered?.Invoke();
+	}
+}
