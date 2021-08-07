670c3f6cc5297d23ebe962a730d812771a9a8e21
Ocelot <ocelotwithmonocle@gmail.com>
2021-08-07 19:43:39 +0700

    связующие ноды в сценарии для удобства

---
+++ src/Assets/Scripts/Systems/Scenario/ScenarioNode.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Scenario
+{
+	/// <summary>
+	/// Nodes serve as middlemen in sequences of scenario scripts to make re-routing simple.
+	/// </summary>
+	public class ScenarioNode : ScenarioScript
+	{
+		public void Activate() => Finish();
+	}
+}
