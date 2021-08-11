2d6c650b03724d3cd8186abe972703048e6d4b7d
Ocelot <ocelotwithmonocle@gmail.com>
2021-08-11 21:37:00 +0700

    таймлайны бок о бок с диалогами, наконец-то

---
+++ src/Assets/Scripts/Systems/Scenario/TimelineAssistant.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Scenario
+{
+	[RequireComponent(typeof(PlayableDirector))]
+	public class TimelineAssistant : MonoBehaviour
+	{
+		private PlayableDirector director;
+
+		private void Awake() =>
+			director = GetComponent<PlayableDirector>();
+
+		public double Speed
+		{
+			get => director.playableGraph.GetRootPlayable(0).GetSpeed();
+			set => director.playableGraph.GetRootPlayable(0).SetSpeed(value);
+		}
+
+		public void Pause() =>
+			Speed = 0;
+
+		public void Resume() =>
+			Speed = 1;
+	}
+}
