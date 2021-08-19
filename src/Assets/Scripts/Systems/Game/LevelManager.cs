a7e489af6b864ed2d28c637576542711672c9239
unknown <apodolyak.99@mail.ru>
2021-08-19 13:18:31 +0700

    Scene changer

---
+++ src/Assets/Scripts/Systems/Game/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+
+public class LevelManager : MonoBehaviour
+{
+	public static LevelManager Instance;
+
+	[SerializeField]
+	private GameObject loaderCanvas;
+	[SerializeField]
+	private Slider progressBar;
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	public async void LoadScene(string sceneName)
+	{
+		var scene = SceneManager.LoadSceneAsync(sceneName);
+		scene.allowSceneActivation = false;
+
+		loaderCanvas.SetActive(true);
+		do
+		{
+			await Task.Delay(100);  //only for test purpose
+			progressBar.value = scene.progress;
+
+		} while (scene.progress < 0.9f);
+
+		scene.allowSceneActivation = true;
+
+		loaderCanvas.SetActive(false);
+	}
+
+}
