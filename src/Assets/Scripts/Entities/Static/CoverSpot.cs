438adbb6964159fb4be629b3741576e45d40a4de
unknown <apodolyak.99@mail.ru>
2021-08-31 19:03:42 +0700

    new cover system

--- src/Assets/Scripts/Entities/Static/CoverSpot.cs
+++ src/Assets/Scripts/Entities/Static/CoverSpot.cs
@@ -55,16 +55,15 @@ public class CoverSpot : MonoBehaviour
 
 	private void CheckSafety()
 	{
-		float detectionRange = 50f;
-		if (Physics.Raycast(transform.position, player.transform.position - transform.position, out RaycastHit hit, detectionRange, layerMask))
+		Vector3 dir = (player.transform.position - transform.position).normalized;
+		float angle = Vector3.SignedAngle(dir, transform.forward, Vector3.up);
+		if (Mathf.Abs(angle) < 90)  //Checking that the new point is not in the target's direction
 		{
-			Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
-			isSafe = true;
+			isSafe = false;
 		}
 		else
 		{
-			Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
-			isSafe = false;
+			isSafe = true;
 		}
 	}
 
