b2b9454ee88b49fe68884d06af3c3a406a7fa536
Ocelot <ocelotwithmonocle@gmail.com>
2021-12-21 18:33:09 +0700

    двери могут быть открыты или закрыты с самого начала

--- src/Assets/Scripts/Entities/Static/Door.cs
+++ src/Assets/Scripts/Entities/Static/Door.cs
@@ -1,34 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class Door : Interaction
 {
 	[field: SerializeField]
 	public float Radius { get; private set; } = 3f;
 
-	[field: SerializeField]
-	public bool Opened { get; private set; } = false;
+	[SerializeField]
+	private bool startOpened;
+
+	private bool __opened = false;
+	public bool Opened
+	{
+		get => __opened;
+		private set
+		{
+			__opened = value;
+			animator.SetBool("IsOpening", __opened);
+		}
+	}
 
 	[field: SerializeField]
 	public bool Locked { get; private set; } = false;
 
+	// Event used to make noises and flash lights to indicate that the door is locked.
+	[SerializeField]
+	private UnityEvent lockEvent;
+
 	[SerializeField]
 	private Animator animator;
+
+	protected void Start() =>
+		Opened = startOpened;
 
 	public override bool CanBeUsedBy(Mob mob) => !Locked;
 
 	public override bool OnUse(Mob mob)
 	{
-		if (Opened)
+		if (Locked)
 		{
-			animator.SetBool("IsOpening", false);
-
-		}
-		else
-		{
-			animator.SetBool("IsOpening", true);
+			lockEvent?.Invoke();
+			return false;
 		}
 
 		Opened = !Opened;
+
 		return true;
 	}
 }
