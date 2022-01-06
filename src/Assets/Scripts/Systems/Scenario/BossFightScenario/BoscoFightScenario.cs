8c02fe77262cd5f6f34b30eea252bf8010f20f71
Freezee <apodolyak.99@mail.ru>
2022-01-06 15:06:57 +0700

    Turret logic

--- src/Assets/Scripts/Systems/Scenario/BossFightScenario/BoscoFightScenario.cs
+++ src/Assets/Scripts/Systems/Scenario/BossFightScenario/BoscoFightScenario.cs
@@ -1,18 +1,69 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BoscoFightScenario : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	[field : SerializeField]
+	private int startStageNumber;
+	[field: SerializeField]
+	private int numberOfDroneWaves;
+	[field: SerializeField]
+	private Mob boss;
+	[field: SerializeField]
+	private Mob bossStage3;
+
+	public int dronesLeft;
+
+	private int currentStageNumber;
+	private int wavesLeft;
+
+	private void Start()
+	{
+		currentStageNumber = startStageNumber;
+		wavesLeft = numberOfDroneWaves;
+	}
+
+	private void Update()
     {
-        
+		switch (currentStageNumber)
+		{
+		case 0:
+			ManageFirstStage();
+			break;
+		case 1:
+			ManageSecondStage();
+			break;
+		case 2:
+			ManageThirdStage();
+			break;
+		case 3:
+			print("BOSCO DEFEATED!");
+			return;
+		}
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void ManageFirstStage()
+	{
+		if (boss.Health < boss.MaxHealth * 0.3f)
+			currentStageNumber = 1;
+	}
+
+	/*
+	 * Спавнит дронов пока не будет уничтожено n волн
+	 */
+	private void ManageSecondStage()
+	{
+		if (wavesLeft > 0 && dronesLeft <= 0)
+		{
+			wavesLeft--;
+			//SpawnWave(); //Найти где
+			//dronesLeft = Сколько дронов заспавнилось
+		}
+	}
+
+	private void ManageThirdStage()
+	{
+
+	}
 }
---
+++ src/Assets/Scripts/Systems/Scenario/BossFightScenario/TurretBehavior.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TurretBehavior : MonoBehaviour
+{
+	[field : SerializeField]
+	private Mob possesed;
+	[field: SerializeField]
+	private Mob target;
+	[field: SerializeField]
+	private float attackRecoveryTime;
+	[field: SerializeField]
+	private float defaultAttackInterval;
+	[field: SerializeField]
+	private float specialAttackInterval;
+
+	private bool attacking = false;
+	private float currentAttackRecoveryTime;
+
+	public bool IsDefaultState = true;
+
+	void Update()
+	{
+		//target dir
+		//aim pos
+		//time between attacks
+		//if default state attack in interval
+		//else attack long
+		Vector3 targetDirection = target.transform.position - possesed.transform.position;
+		possesed.AimPos = target.transform.position + Vector3.up * possesed.AimHeight;
+		possesed.TurnTo(Time.deltaTime, possesed.AimDir);
+		if (IsDefaultState)
+		{
+			if (currentAttackRecoveryTime <= 0)
+			{
+				if (!attacking)
+					StartCoroutine(waiter(defaultAttackInterval));
+				else
+					possesed.UseItem(true);
+			}
+		}
+		else
+		{
+			if (!attacking)
+				StartCoroutine(waiter(specialAttackInterval));
+			else
+				possesed.UseItem(true);
+		}
+		
+		HandleRecoveryTime();
+	}
+
+	private IEnumerator attackSequence(float time)
+	{
+		attacking = true;
+		float counter = 0;
+		while (counter < time)
+		{
+			counter += Time.deltaTime;
+			yield return null;
+		}
+
+		currentAttackRecoveryTime = attackRecoveryTime;
+		attacking = false;
+		possesed.UseItem(false);
+	}
+
+	private IEnumerator waiter(float time)
+	{
+		yield return attackSequence(time);
+	}
+
+	private void HandleRecoveryTime()
+	{
+		if (currentAttackRecoveryTime > 0)
+		{
+			currentAttackRecoveryTime -= Time.deltaTime;
+		}
+	}
+}
+
+
