f53814d12e83c836ff5056d719aec1b8f404bf4b
Freezee <apodolyak.99@mail.ru>
2022-01-06 17:25:31 +0700

    Stage 3

---
+++ src/Assets/Scripts/Systems/Scenario/BossFightScenario/BoscoTurret.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BoscoTurret : MonoBehaviour
+{
+	[field: SerializeField]
+	private Mob possesed;
+	[field: SerializeField]
+	private Mob target;
+	[field: SerializeField]
+	private float attackRecoveryTime;
+	[field: SerializeField]
+	private float valleyRecoveryTime;
+	[field: SerializeField]
+	private float machinegunAttackInterval;
+
+
+	private bool attacking = false;
+	private bool reloading = false;
+	private float currentAttackRecoveryTime;
+	private float currentValleyRecoveryTime;
+	private float currentMachinegunRecoveryTime;
+
+	public bool IsValleyState = false;
+	public bool IsMachinegunState = true;
+
+	void Update()
+	{
+		if (IsMachinegunState)
+		{
+			Vector3 targetDirection = target.transform.position - possesed.transform.position;
+			possesed.AimPos = target.transform.position + Vector3.up * possesed.AimHeight;
+			possesed.TurnTo(Time.deltaTime, possesed.AimDir);
+			if (currentMachinegunRecoveryTime <= 0)
+			{
+				if (!attacking)
+					StartCoroutine(waiter(machinegunAttackInterval));
+				else
+					possesed.UseItem(true);
+			}
+		}
+		if (IsValleyState)
+		{
+			if (currentValleyRecoveryTime <= 0)
+			{
+				LaunchValley();
+				currentValleyRecoveryTime = valleyRecoveryTime;
+			}
+		}
+
+		HandleRecoveryTime();
+	}
+
+	private IEnumerator attackSequence(float time)
+	{
+		attacking = true;
+		float counter = 0;
+		while (counter < time)
+		{
+			counter += Time.deltaTime;
+			yield return null;
+		}
+
+		currentAttackRecoveryTime = attackRecoveryTime;
+		attacking = false;
+		possesed.UseItem(false);
+	}
+
+	private IEnumerator waiter(float time)
+	{
+		yield return attackSequence(time);
+	}
+
+	private void HandleRecoveryTime()
+	{
+		if (currentAttackRecoveryTime > 0)
+		{
+			currentAttackRecoveryTime -= Time.deltaTime;
+		}
+
+		if (currentMachinegunRecoveryTime > 0)
+		{
+			currentMachinegunRecoveryTime -= Time.deltaTime;
+		}
+
+		if (currentValleyRecoveryTime > 0)
+		{
+			currentValleyRecoveryTime -= Time.deltaTime;
+		}
+	}
+
+	//spawn new dangerous zone
+	private void LaunchValley()
+	{
+		
+	}
+}
+
+
