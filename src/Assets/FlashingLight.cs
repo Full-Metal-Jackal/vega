f04e4cfb13ba9e8eb3a16b940ffc7a1cb7fcaf83
Ocelot <ocelotwithmonocle@gmail.com>
2022-01-14 23:44:38 +0700

    добавление проджектайла мины

---
+++ src/Assets/FlashingLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour
+{
+	[SerializeField]
+	private Light flashingLight;
+
+	[SerializeField]
+	private float flashingSpeed = 10f;
+
+	[SerializeField]
+	private float maxIntensity = 1f;
+
+	private void Update()
+	{
+		if (!flashingLight)
+			return;
+
+		flashingLight.intensity = Mathf.Sin(Time.time * flashingSpeed) * maxIntensity;
+	}
+}
---
+++ src/Assets/MineProjectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineProjectile : GrenadeProjectile
+{
+	[SerializeField]
+	private float activationDelay = 2f;
+	private bool activated = false;
+
+	[SerializeField]
+	private Event onTriggered;
+
+	[SerializeField]
+	private float triggerRadius = 1f;
+	private bool triggered = false;
+
+	protected override void Update()
+	{
+		float delta = Time.deltaTime;
+
+		if (!activated)
+		{
+			activated = (activationDelay -= delta) < 0;
+			return;
+		}
+
+		if (!triggered)
+			return;
+		base.Update();
+	}
+
+	public void Trigger()
+	{
+		triggered = true;
+	}
+}
