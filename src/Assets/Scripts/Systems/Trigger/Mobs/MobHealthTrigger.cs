0b2e50e170a74de334ed79a01bac65c6794cb016
Ocelot <ocelotwithmonocle@gmail.com>
2022-07-07 16:20:49 +0700

    коммит для переноса на пк

---
+++ src/Assets/Scripts/Systems/Trigger/Mobs/MobHealthTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TriggerSystem;
+
+public class MobHealthTrigger : Trigger
+{
+	[SerializeField]
+	private Mob mob;
+
+	[SerializeField]
+	private float health = 30f;
+
+	private void Awake()
+	{
+		mob.OnDamaged += RegisterDeath;
+	}
+
+	private void RegisterDeath(Mob mob)
+	{
+		mob.OnDamaged -= RegisterDeath;
+
+		Activate();
+	}
+}
